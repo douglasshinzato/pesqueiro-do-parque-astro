f270d477a835cc64d2b902c83d30720891608d58
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-05-09 15:46:13 -0400

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeInDown: "fadeInDown 1s ease-out",
+        fadeInUp: "fadeInUp 1s ease-out",
+      },
+      keyframes: {
+        fadeInDown: {
+          "0%": { opacity: 0, transform: "translateY(-100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        fadeInUp: {
+          "0%": { opacity: 0, transform: "translateY(100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        kalam: ["Kalam", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+}
